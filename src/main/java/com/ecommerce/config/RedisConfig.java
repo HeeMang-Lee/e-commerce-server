e02b174c55120f78522d94a395e9972ea70ad1b4
hemsej018 <hemsej018@naver.com>
2025-11-26 11:43:07 +0900

    feat: Redis & Redisson 의존성 및    설정 추가

---
+++ src/main/java/com/ecommerce/config/RedisConfig.java
@@ -0,0 +1,30 @@
+package com.ecommerce.config;
+
+import org.redisson.Redisson;
+import org.redisson.api.RedissonClient;
+import org.redisson.config.Config;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Redis 및 Redisson 설정
+ */
+@Configuration
+public class RedisConfig {
+
+    @Value("${spring.data.redis.host}")
+    private String redisHost;
+
+    @Value("${spring.data.redis.port}")
+    private int redisPort;
+
+    @Bean
+    public RedissonClient redissonClient() {
+        Config config = new Config();
+        config.useSingleServer()
+                .setAddress("redis://" + redisHost + ":" + redisPort);
+
+        return Redisson.create(config);
+    }
+}
